15a2872d76c619425a54fadbf30610a4df8da467
Luca Lo Forte <pizzulao@gmail.com>
2025-02-15 14:37:18 +0100

    feat: read input from terminal and print out first word of the sentence

--- main.go
+++ main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		for scanner.Scan() {
+			firstWord := cleanInput(scanner.Text())
+			fmt.Printf("Your command was: %s \n", firstWord[0])
+			break
+		}
+	}
 }
 
 func cleanInput(text string) []string {
 	trimmedText := strings.TrimSpace(text)
-	splittedSlice := strings.Split(trimmedText, " ")
+	lowerCaseText := strings.ToLower(trimmedText)
+	splittedSlice := strings.Split(lowerCaseText, " ")
 	return splittedSlice
 }
