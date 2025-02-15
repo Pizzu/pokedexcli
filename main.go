335b3118d0a4b4d0ac9694c32f34cf5abdbeb622
Luca Lo Forte <pizzulao@gmail.com>
2025-02-15 14:42:13 +0100

    feat: modify main func

--- main.go
+++ main.go
@@ -11,11 +11,17 @@ func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
-		for scanner.Scan() {
-			firstWord := cleanInput(scanner.Text())
-			fmt.Printf("Your command was: %s \n", firstWord[0])
-			break
+		scanner.Scan()
+
+		words := cleanInput(scanner.Text())
+
+		if len(words) == 0 {
+			continue
 		}
+
+		commandName := words[0]
+
+		fmt.Printf("Your command was: %s \n", commandName)
 	}
 }
 
